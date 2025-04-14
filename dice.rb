347e00ad7ced1ac1089f6e650e4395435bba547d
yzemlyak <yzemlyak@gmail.com>
2025-04-14 22:33:17 +0000

    Added 100_6 route

--- dice.rb
+++ dice.rb
@@ -43,9 +43,9 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @die = rand(1..20)
   	
-  @outcome = "You rolled a #{first_die} for a total of #{first_die}."
+  @outcome = "You rolled a #{@die}."
 	
   erb(:one_twenty)
 end
@@ -61,4 +61,14 @@ get("/dice/5/4") do
   @outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
 	
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
