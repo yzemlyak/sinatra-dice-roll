758d8b82875c3c7618db28a8fe9df3a2c08ed6ba
yzemlyak <yzemlyak@gmail.com>
2025-04-14 20:38:16 +0000

    Added first 2-die roll

--- dice.rb
+++ dice.rb
@@ -11,3 +11,14 @@ end
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
