30a4244b2c6e73510f812395c7650ca8adb35b68
yzemlyak <yzemlyak@gmail.com>
2025-04-14 20:54:54 +0000

    Current before links

--- dice.rb
+++ dice.rb
@@ -22,3 +22,37 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  	
+  outcome = "You rolled a #{first_die} for a total of #{first_die}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
+end
