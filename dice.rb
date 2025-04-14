07ac796220469da11f715a503774021d4649128d
yzemlyak <yzemlyak@gmail.com>
2025-04-14 20:35:07 +0000

    Added fast refresh

--- dice.rb
+++ dice.rb
@@ -1,4 +1,13 @@
 require "sinatra"
+require "sinatra/reloader"
+get("/") do
+  "Hello world"
+end
+
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
